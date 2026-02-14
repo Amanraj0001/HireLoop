c6208fb6c52cfd3b8647329bb1bf126c877a663c
Ayush Das <ayushdas7354@gmail.com>
2026-02-14 16:58:27 +0530

    fixed server

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,7 +8,6 @@ const app = express();
 // basic configurations
 app.use(express.json({ limit: "16kb"}))
 app.use(express.urlencoded({ extended: true, limit: "16kb"}))
-app.use(express.static("public"))
 app.use("/api/v1/auth", authRoutes)
 
 // cors configuration
