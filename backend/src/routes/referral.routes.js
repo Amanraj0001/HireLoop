d8c327740c7627f9f31134ca49cb698767acd957
Raj-aman-1 <rajamannitjsr.gmail.com>
2026-02-23 20:29:41 +0530

    added router for updatereferralstatus

--- backend/src/routes/referral.routes.js
+++ backend/src/routes/referral.routes.js
@@ -1,9 +1,12 @@
 import { Router } from "express";
 import verifyToken from "../middlewares/auth.middlewares.js";
 import { requestReferral } from "../controllers/referral.controllers.js";
+import { updateReferralStatus } from "../controllers/referral.controllers.js";
+import authMiddleware from "../middlewares/auth.middlewares.js"
 
 const router=Router();
 
 router.post("/request",verifyToken,requestReferral);
+router.put("/:id/:status", authMiddleware, updateReferralStatus);
 
 export default router;
